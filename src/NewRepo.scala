918c34767d22711ccee44884cadd2e9b9bf7eef3
samthrower64 <samthrower0304@gmail.com>
2022-10-04 11:37:48 +0100

    String Syntax examples for length return, concatenation, return characters at location x, convert to upper case.

--- src/NewRepo.scala
+++ src/NewRepo.scala
@@ -1,3 +1,13 @@
-object NewRepo extends App{
-  println("Testing for New Repository")
+object NewRepo{
+  def main(args: Array[String]): Unit = {
+    var greet: String = "Hello"
+    println(greet.length())
+    //5
+    println(greet.concat("Scala"))
+    //Hello Scala
+    println(greet.charAt(1))
+    //e
+    println(greet.toUpperCase())
+    //HELLO
+  }
 }
